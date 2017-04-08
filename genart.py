5258b2c39488bdd7004537af11c8467d163fd6d3
Abhishek Mangla <amangla@berkeley.edu>
2017-04-08 13:21:45 -0700

    finished

--- genart.py
+++ genart.py
@@ -1,26 +1,44 @@
 #Searching and Downloading Google Images/Image Links
 #https://github.com/hardikvasa/google-images-download/blob/master/google-images-download.py
-#Import Libraries
-#use python3.5 genart.py
-#!/usr/bin/python3
 
-import time       #Importing the time library to check the time of code execution
-import sys    #Importing the System Library
+#pip3 install numpy
+#pip3 install Pillow
+#pip3 install -U scikit-image
+#to run: python3 genart.py
+
+import time
+import sys
 import urllib.request
 import numpy as np
 from PIL import Image
 from urllib.request import Request, urlopen
 from urllib.request import URLError, HTTPError
+import priorityQueue
+import itertools
+# from multiprocessing import Pool
+# from functools import partial
+from skimage.measure import compare_ssim as ssim
 
-########### Edit From Here ###########
+def mse(imageA, imageB):
+    # the 'Mean Squared Error' between the two images is the
+    # sum of the squared difference between the two images;
+    # NOTE: the two images must have the same dimension
+    lst1 = list(imageA.getdata())
+    lst1_flattened = list(itertools.chain(*lst1))
 
-#This list is used to search keywords. You can edit this list to search for google images of your choice. You can simply add and remove elements of the list.
-search_keyword = ["Rose", "Blue"]
-
-#This list is used to further add suffix to your search term. Each element of the list will help you download 100 images. First element is blank which denotes that no suffix is added to the search keyword of the above list. You can edit the list by adding/deleting elements from it.So if the first element of the search_keyword is 'Australia' and the second element of keywords is 'high resolution', then it will search for 'Australia High Resolution'
-keywords = ["high resolution", "Blue"]
-
-########### End of Editing ###########
+    lst2 = list(imageB.getdata())
+    lst2_flattened = list(itertools.chain(*lst2))
+    if len(lst1_flattened) != len(lst2_flattened):
+        # print('error')
+        return 0
+    res = (np.array(lst1_flattened) - np.array(lst2_flattened)) ** 2
+    # print(res)
+    err = np.sum(res)
+    err /= float(imageA.size[0] * imageA.size[1])
+    
+    # return the MSE, the lower the error, the more "similar"
+    # the two images are
+    return err
 
 #Downloading entire Web Document (Raw Page Content)
 def download_page(url):
@@ -81,86 +99,153 @@ def _images_get_all_items(page):
 
 
 ############## Main Program ############
-t0 = time.time()   #start the timer
+def prelim():
+    t0 = time.time()   #start the timer
 
-#Download Image Links
-items = []
-search = ""
-#not using keywords
-for keyword in search_keyword:
-    pure_keyword = keyword.replace(' ','%20')
-    search += pure_keyword
+    #Download Image Links
+    items = []
+    search = ""
+    #not using keywords
+    for keyword in search_keyword:
+        pure_keyword = keyword.replace(' ','%20')
+        search += pure_keyword
 
-url = 'https://www.google.com/search?q=' + search + '&espv=2&biw=1366&bih=667&site=webhp&source=lnms&tbm=isch&sa=X&ei=XosDVaCXD8TasATItgE&ved=0CAcQ_AUoAg'
-raw_html =  (download_page(url))
-time.sleep(0.1)
-items = (_images_get_all_items(raw_html))
-#print ("Image Links = "+str(items))
-print ("Total Image Links = "+str(len(items)))
-print ("\n")
+    url = 'https://www.google.com/search?q=' + search + '&espv=2&biw=1366&bih=667&site=webhp&source=lnms&tbm=isch&sa=X&ei=XosDVaCXD8TasATItgE&ved=0CAcQ_AUoAg'
+    raw_html =  (download_page(url))
+    time.sleep(0.1)
+    items = (_images_get_all_items(raw_html))
+    #print ("Image Links = "+str(items))
+    print ("Total Image Links = "+str(len(items)))
+    print ("\n")
 
 
-#This allows you to write all the links into a test file. This text file will be created in the same directory as your code. You can comment out the below 3 lines to stop writing the output to the text file.
-# info = open('output.txt', 'a')        #Open the text file called database.txt
-# info.write(str(i) + ': ' + str(search_keyword[i-1]) + ": " + str(items) + "\n\n\n")         #Write the title of the page
-# info.close()                            #Close the file
+    #This allows you to write all the links into a test file. This text file will be created in the same directory as your code. You can comment out the below 3 lines to stop writing the output to the text file.
+    # info = open('output.txt', 'a')        #Open the text file called database.txt
+    # info.write(str(i) + ': ' + str(search_keyword[i-1]) + ": " + str(items) + "\n\n\n")         #Write the title of the page
+    # info.close()                            #Close the file
 
-t1 = time.time()    #stop the timer
-total_time = t1-t0   #Calculating the total time required to crawl, find and download all the links of 60,000 images
-print("Total time taken: "+str(total_time)+" Seconds")
-print ("Starting Download...")
+    t1 = time.time()    #stop the timer
+    total_time = t1-t0   #Calculating the total time required to crawl, find and download all the links of 60,000 images
+    print("Total time taken: "+str(total_time)+" Seconds")
+    print ("Starting Download...")
 
-## To save imges to the same directory
-# IN this saving process we are just skipping the URL if there is any error
+    ## To save imges to the same directory
+    # IN this saving process we are just skipping the URL if there is any error
 
-k=0
-errorCount=0
-while(k<len(items)):
-# while(k<10):
-    try:
-        req = Request(items[k], headers={"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
-        response = urlopen(req)
-        output_file = open(str(k+1) + ".jpg","wb")
-        # image = Image.open(str(k+1)+".jpg")
-        # print(image)
+    k=0
+    errorCount=0
+    while(k < min(5, len(items))):
+        try:
+            req = Request(items[k], headers={"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
+            response = urlopen(req)
+            output_file = open(str(k+1) + ".jpg","wb")
+            data = response.read()
+            output_file.write(data)
+            response.close();
+            image = Image.open(str(k+1)+".jpg")
+            images[str(k+1)+".jpg"] = image
+            print("completed ====> "+str(k+1))
+            k=k+1;
+        except IOError:   #If there is any IOError
+            errorCount+=1
+            print("IOError on image "+str(k+1))
+            k=k+1;
+        except HTTPError as e:  #If there is any HTTPError
+            errorCount+=1
+            print("HTTPError"+str(k))
+            k=k+1;
+        except URLError as e:
+            errorCount+=1
+            print("URLError "+str(k))
+            k=k+1;
+
+    print("\n")
+    print("All are downloaded")
+    print("\n"+str(errorCount)+" ----> total Errors")
+
+def computeSimilarity(orig_image, images):
+    running_sum = 0
+    for imageName in images.keys():
+        image = images[imageName]
+        orig_image_size = orig_image.size
+        running_sum += mse(orig_image, image.resize(orig_image_size))
+        # lst1 = list(orig_image.getdata())
+        # lst1_flattened = list(itertools.chain(*lst1))
+        # # lst1_flattened = np.array(lst1_flattened)
+        # lst2 = list(image.resize(orig_image_size).getdata())
+        # lst2_flattened = list(itertools.chain(*lst2))
+        # lst2_flattened = np.array(lst2_flattened)
+        # running_sum += ssim(lst1_flattened, lst2_flattened)
+    print("computed similarity")
+    return running_sum / len(images.keys())
+
+def create_collage(width, height, listofimages):
+    cols = 3
+    rows = 1
+    thumbnail_width = width//cols
+    thumbnail_height = height//rows
+    size = thumbnail_width, thumbnail_height
+    new_im = Image.new('RGB', (width, height))
+    ims = []
+    for p in listofimages:
+        im = Image.open(p)
+        im.thumbnail(size)
+        ims.append(im)
+    i = 0
+    x = 0
+    y = 0
+    for col in range(cols):
+        for row in range(rows):
+            print(i, x, y)
+            new_im.paste(ims[i], (x, y))
+            i += 1
+            y += thumbnail_height
+        x += thumbnail_width
+        y = 0
+
+    new_im.save("Collage.jpg")
+
+
+if __name__ == '__main__':
+    #This list is used to search keywords. You can edit this list to search for google images of your choice. You can simply add and remove elements of the list.
+    oi = input("Enter your object of interest: ")
+    adj1 = input("Adjective 1: ")
+    search_keyword = [oi]
+    # pool = Pool(processes=6)
+    images = {}
+    pq = priorityQueue.PriorityQueue()
+    #This list is used to further add suffix to your search term. Each element of the list will help you download 100 images. First element is blank which denotes that no suffix is added to the search keyword of the above list. You can edit the list by adding/deleting elements from it.So if the first element of the search_keyword is 'Australia' and the second element of keywords is 'high resolution', then it will search for 'Australia High Resolution'
+    keywords = ["high resolution", "Blue"]
+    prelim()
+    for imageName in images.keys():
+        image = images[imageName]
+        pq.update(imageName, computeSimilarity(image, images))
+        # args.append(image)
+        # order.append(imageName)
+
+    # partial_harvester = partial(computeSimilarity, images=images)
+    # result_similarities = pool.map(partial_harvester, args, 1)
+    # pool.close()
+    # pool.join()
+
+    # assert len(order) == len(args)
+    # for i in range(len(order)):
+    #     imageName = order[i]
+    #     similarity = result_similarities[i]
+    #     pq.update(imageName, similarity)
+    order = []
+    for i in range(3):
+        imageName = pq.pop()[1]
+        print(i, " : ", imageName)
+        order.append(imageName)
+        # image = Image.open(imageName)
         # image.show()
-        # img = cv2.imread(output_file,0)
-        # cv2.namedWindow(output_file, cv2.WINDOW_NORMAL)
-        # cv2.imshow(output_file,img)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
 
-        data = response.read()
-        output_file.write(data)
-        response.close();
+    create_collage(450, 300, order)
+    image = Image.open("Collage.jpg")
+    image.show()
 
-        print("completed ====> "+str(k+1))
-
-        k=k+1;
-
-    except IOError:   #If there is any IOError
-
-        errorCount+=1
-        print("IOError on image "+str(k+1))
-        k=k+1;
-
-    except HTTPError as e:  #If there is any HTTPError
-
-        errorCount+=1
-        print("HTTPError"+str(k))
-        k=k+1;
-    except URLError as e:
-
-        errorCount+=1
-        print("URLError "+str(k))
-        k=k+1;
-
-print("\n")
-print("All are downloaded")
-print("\n"+str(errorCount)+" ----> total Errors")
-
-# image = Image.open(str(1)+".jpg")
-# image.show()
-
+#idea: get top 10 best representative images for the search query based on similarity with the
+#100 responses, then make a collage of these with biggest representing best, smallest representing worst
 
 #----End of the main program ----#
